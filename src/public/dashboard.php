76863b9d75ad803fb56d504dd3026370c3204d9f
Dário Jr <53487868+BirdRa1n@users.noreply.github.com>
2025-10-28 09:56:34 -0300

    chore: remover arquivos PHP obsoletos e limpar rotas

--- src/public/dashboard.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once '../app/auth.php';
-require_once '../config/database.php';
-require_once '../app/Controllers/DashboardController.php';
-
-$controller = new DashboardController($pdo);
-$controller->index();
